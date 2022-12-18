64ba462a80c5f41aabfc6c87080a92fa144217e0
ウネル <117335242+unel-app@users.noreply.github.com>
2022-12-18 10:33:29 +0600

    Create money-bouquet.kt

---
+++ server/dub/episode/money-bouquet.kt
@@ -0,0 +1,73 @@
+[ {
+"episode":"1 бөлім",
+"is":"https://anifon-nurislam003.koyeb.app/AgAD3x338"
+},
+{"episode":"2 бөлім",
+"is":"https://anifon-nurislam003.koyeb.app/AgAD3R342"
+},
+{"episode":"3 бөлім",
+"is":"https://anifon-nurislam003.koyeb.app/AgADCh344"
+},
+{"episode":"4 бөлім",
+"is":"https://anifon-nurislam003.koyeb.app/AgADCB340"
+},
+{"episode":"5 бөлім",
+"is":"https://anifon-nurislam003.koyeb.app/AgADCR346""
+},
+{"episode":"6 бөлім",
+"is":"https://anifon-nurislam003.koyeb.app/AgADEB348"
+},
+{"episode":"7 бөлім",
+"is":"https://anifon-nurislam003.koyeb.app/AgADeR350"
+},
+{"episode":"8 бөлім",
+"is":"https://anifon-nurislam003.koyeb.app/AgADfh352"
+},
+{"episode":"9 бөлім",
+"is":"https://anifon-nurislam003.koyeb.app/AgADdR354"
+},
+{"episode":"10 бөлім",
+"is":"https://anifon-nurislam003.koyeb.app/AgADcx356"
+},
+{"episode":"11 бөлім",
+"is":""
+},
+{"episode":"12 бөлім",
+"is":""
+},
+{"episode":"13 бөлім",
+"is":""
+},
+{"episode":"14 бөлім",
+"is":""
+},
+{"episode":"15 бөлім",
+"is":""
+},
+{"episode":"16 бөлім",
+"is":""
+},
+{"episode":"17 бөлім",
+"is":""
+},
+{"episode":"18 бөлім",
+"is":""
+},
+{"episode":"19 бөлім",
+"is":""
+},
+{"episode":"20 бөлім",
+"is":""
+},
+{"episode":"21 бөлім",
+"is":""
+},
+{"episode":"22 бөлім",
+"is":""
+},
+{"episode":"23 бөлім",
+"is":""
+},
+{"episode":"24 бөлім",
+"is":""
+} ]
