59d69ae992a699676f9f3c2ae1040b80dfe3c887
ウネル <117335242+unel-app@users.noreply.github.com>
2022-12-21 13:29:49 +0600

    Update chainsaw-man.kt

--- server/dub/episode/chainsaw-man.kt
+++ server/dub/episode/chainsaw-man.kt
@@ -25,4 +25,7 @@
 },
 {"episode":"9 бөлім",
 "is":"https://ok.ru/videoembed/4463186086601"
+},
+{"episode":"Test 10",
+"is":"https://6mwyl50nbll.a.trbcdn.net/livemaster/u7v3o_live-i59ya936e5f.smil/playlist.m3u8&amp;su=true#amp=1"
 } ]
