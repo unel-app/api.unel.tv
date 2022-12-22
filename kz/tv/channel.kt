cbd9b7d7efc1af99b2e9e6c270a1f1ce18c0d220
ウネル <117335242+unel-app@users.noreply.github.com>
2022-12-22 09:21:21 +0600

    Update channel.kt

--- kz/tv/channel.kt
+++ kz/tv/channel.kt
@@ -1,4 +1,7 @@
 [ {
 "img":"https://upload.wikimedia.org/wikipedia/commons/2/27/Channel_31_logo_2020.png",
 "title":"31 арна"
+},
+{"img":"https://upload.wikimedia.org/wikipedia/commons/2/27/Channel_31_logo_2020.png",
+"title":"7 арна"
 } ]
