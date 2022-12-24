6220a2a715572b0d9659d0b4125f63319264a488
ウネル <117335242+unel-app@users.noreply.github.com>
2022-12-24 16:16:21 +0600

    Update channel.kt

--- kz/tv/channel.kt
+++ kz/tv/channel.kt
@@ -2,5 +2,6 @@
 "tv_img":"https://static.wikia.nocookie.net/tvpedia/images/4/44/31_%D0%BA%D0%B0%D0%BD%D0%B0%D0%BB_%D0%9A%D0%B0%D0%B7%D0%B0%D1%85%D1%81%D1%82%D0%B0%D0%BD_%28%D1%81_2020%2C_%D0%BC%D0%B8%D0%BA%D1%80%D0%BE%D1%84%D0%BE%D0%BD%29.png/revision/latest/scale-to-width-down/250?cb=20210516203752&path-prefix=ru",
 "tv_name":"31-ші арна",
 "tv_live":"• ТІКЕЛЕЙ ЭФИР",
-"tv_desc":"31-ші арна — Қазақ және орыс тілінде телебағдарламаларды көруге арналған арна"
+"tv_desc":"31-ші арна — Қазақ және орыс тілінде телебағдарламаларды көруге арналған арна",
+"id":"31-channel"
 } ]
