0150fcd161083385c3883ce6fb097d1bef8dfd13
ウネル <117335242+unel-app@users.noreply.github.com>
2022-12-28 20:47:38 +0600

    Create k2.kt

---
+++ server/dub/episode/k2.kt
@@ -0,0 +1,49 @@
+[ {
+"episode":"1 бөлім",
+"is":"https://anifon-nurislam003.koyeb.app/AgADZy777"
+},
+{"episode":"2 бөлім",
+"is":"https://anifon-nurislam003.koyeb.app/AgADai779"
+},
+{"episode":"3 бөлім",
+"is":"https://anifon-nurislam003.koyeb.app/AgADbS781"
+},
+{"episode":"4 бөлім",
+"is":"https://anifon-nurislam003.koyeb.app/AgADXS783"
+},
+{"episode":"5 бөлім",
+"is":"https://anifon-nurislam003.koyeb.app/AgADdi785"
+},
+{"episode":"6 бөлім",
+"is":"https://anifon-nurislam003.koyeb.app/AgADei787"
+},
+{"episode":"7 бөлім",
+"is":"https://anifon-nurislam003.koyeb.app/AgADfC789"
+},
+{"episode":"8 бөлім",
+"is":"https://anifon-nurislam003.koyeb.app/AgADhC791"
+},
+{"episode":"9 бөлім",
+"is":"https://anifon-nurislam003.koyeb.app/AgADgi793"
+},
+{"episode":"10 бөлім",
+"is":"https://anifon-nurislam003.koyeb.app/AgADki795"
+},
+{"episode":"Қол жетімсіз / Недоступно",
+"is":"https://error404"
+},
+{"episode":"12 бөлім",
+"is":"https://anifon-nurislam003.koyeb.app/AgADii799"
+},
+{"episode":"13 бөлім",
+"is":"https://anifon-nurislam003.koyeb.app/AgADpC801"
+},
+{"episode":"14 бөлім",
+"is":"https://anifon-nurislam003.koyeb.app/AgADqS803"
+},
+{"episode":"15 бөлім",
+"is":"https://anifon-nurislam003.koyeb.app/AgADqi805"
+},
+{"episode":"16 бөлім",
+"is":"https://anifon-nurislam003.koyeb.app/AgADRS807"
+} ]
