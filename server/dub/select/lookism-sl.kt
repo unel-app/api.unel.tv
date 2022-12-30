d562cab0571d21d85968f8c233791e8fd5eb7a1e
ウネル <117335242+unel-app@users.noreply.github.com>
2022-12-30 16:52:52 +0600

    Update lookism-sl.kt

--- server/dub/select/lookism-sl.kt
+++ server/dub/select/lookism-sl.kt
@@ -1,7 +1,11 @@
 [ {
-"dub":"AniLibria",
+"dub":"AniLibria (720p)",
 "git":"5 эп.",
 "id":"lookism-id2"
+},
+{"dub":"AniLibria (480p)",
+"git":"5 эп.",
+"id":"lookism-id3"
 },
 {"dub":"AniStar",
 "git":"8 эп.",
