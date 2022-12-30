abbfa2e7862af0482692d06c586832f89d836558
ウネル <117335242+unel-app@users.noreply.github.com>
2022-12-30 17:13:51 +0600

    Update chainsaw-man-id2.kt

--- server/dub/episode/chainsaw-man-id2.kt
+++ server/dub/episode/chainsaw-man-id2.kt
@@ -1,4 +1,7 @@
 [ {
 "episode":"1 бөлім",
 "is":"https://video.sibnet.ru/shell.php?videoid=4969051"
+},
+{"episode":"2 бөлім",
+"is":"https://r190102.kujo-jotaro.com/kage-no-jitsuryokusha/1.720.696462bf80705fcd.mp4"
 } ]
