7595ef9b9f59a66bad314b8c8d18e2d585d85a0e
ウネル <117335242+unel-app@users.noreply.github.com>
2022-12-31 12:04:20 +0600

    Create sikago-tajagi.kt

---
+++ server/dub/episode/sikago-tajagi.kt
@@ -0,0 +1,49 @@
+[ {
+"episode":"1 бөлім",
+"is":"https://anifon-nurislam003.koyeb.app/AgADsS817"
+},
+{"episode":"2 бөлім",
+"is":"https://anifon-nurislam003.koyeb.app/AgADry815"
+},
+{"episode":"3 бөлім",
+"is":"https://anifon-nurislam003.koyeb.app/AgADzC821"
+},
+{"episode":"4 бөлім",
+"is":"https://anifon-nurislam003.koyeb.app/AgAD1S825"
+},
+{"episode":"5 бөлім",
+"is":"https://anifon-nurislam003.koyeb.app/AgAD0S822"
+},
+{"episode":"6 бөлім",
+"is":"https://anifon-nurislam003.koyeb.app/AgADMi882"
+},
+{"episode":"7 бөлім",
+"is":"https://anifon-nurislam003.koyeb.app/AgADxy819"
+},
+{"episode":"8 бөлім",
+"is":"https://anifon-nurislam003.koyeb.app/AgAD9i841"
+},
+{"episode":"9 бөлім",
+"is":"https://anifon-nurislam003.koyeb.app/AgAD6i829"
+},
+{"episode":"10 бөлім",
+"is":"https://anifon-nurislam003.koyeb.app/AgAD9C839"
+},
+{"episode":"11 бөлім",
+"is":"https://anifon-nurislam003.koyeb.app/AgAD9y843"
+},
+{"episode":"12 бөлім",
+"is":"https://anifon-nurislam003.koyeb.app/AgAD7y837"
+},
+{"episode":"13 бөлім",
+"is":"https://anifon-nurislam003.koyeb.app/AgAD7S835"
+},
+{"episode":"14 бөлім",
+"is":"https://anifon-nurislam003.koyeb.app/AgAD7C833"
+},
+{"episode":"15 бөлім",
+"is":"https://anifon-nurislam003.koyeb.app/AgAD6y831"
+},
+{"episode":"16 бөлім",
+"is":"https://anifon-nurislam003.koyeb.app/AgAD5y827"
+} ]
