e3f64855a77d1aff440892b1c025ad0c3eff1963
ウネル <117335242+unel-app@users.noreply.github.com>
2023-01-11 19:21:07 +0600

    Delete poster.py

--- server/poster.py
+++
@@ -1,7 +0,0 @@
-[ {
-"poster":"https://img1.ak.crunchyroll.com/i/spire4/f91275e6e5a3c15be90358c6ead433ca1659084308_main.png"
-},
-{"poster":"https://img1.ak.crunchyroll.com/i/spire4/f91275e6e5a3c15be90358c6ead433ca1659084308_main.png"
-},
-{"poster":"https://img1.ak.crunchyroll.com/i/spire4/f91275e6e5a3c15be90358c6ead433ca1659084308_main.png"
-} ]
