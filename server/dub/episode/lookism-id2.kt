143b2a994b2080e21f7692e5a06f199fdf5b6b97
ウネル <117335242+unel-app@users.noreply.github.com>
2023-01-13 17:31:03 +0600

    Update lookism-id2.kt

--- server/dub/episode/lookism-id2.kt
+++ server/dub/episode/lookism-id2.kt
@@ -13,4 +13,13 @@
 },
 {"episode":"5 бөлім",
 "is":"https://cache.libria.fun/videos/media/ts/9394/5/720/4282cfaa313e67f879f79e204a16bd84.m3u8"
+},
+{"episode":"6 бөлім",
+"is":"https://cache.libria.fun/videos/media/ts/9394/6/720/6cca8e5bed295c7a07319d83495bc0d7.m3u8"
+},
+{"episode":"7 бөлім",
+"is":"https://cache.libria.fun/videos/media/ts/9394/7/720/7c73dedf7dfc66f3ab66be6e09918040.m3u8"
+},
+{"episode":"8 бөлім",
+"is":"https://cache.libria.fun/videos/media/ts/9394/8/720/15ddfa5ede191c7a8e04c6ac9193d807.m3u8"
 } ]
