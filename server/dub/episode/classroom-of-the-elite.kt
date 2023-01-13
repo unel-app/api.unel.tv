72d1342ad3fdae15a7344d53cdd4b893ea540f44
ウネル <117335242+unel-app@users.noreply.github.com>
2023-01-13 17:15:06 +0600

    Create classroom-of-the-elite.kt

---
+++ server/dub/episode/classroom-of-the-elite.kt
@@ -0,0 +1,37 @@
+[ {
+"episode":"1 бөлім",
+"is":"https://cache.libria.fun/videos/media/ts/4578/1/720/cc0cc65617e3ce47490110e4bd727551.m3u8"
+},
+{"episode":"2 бөлім",
+"is":"https://cache.libria.fun/videos/media/ts/4578/2/720/f6b2ac95751cacce1f569cb2fe29d8a0.m3u8"
+},
+{"episode":"3 бөлім",
+"is":"https://cache.libria.fun/videos/media/ts/4578/3/720/9ced4787ad8c6c723160d54fb9e5626b.m3u8"
+},
+{"episode":"4 бөлім",
+"is":"https://cache.libria.fun/videos/media/ts/4578/4/720/4fa5795e03ac8e40d61ff90fdb4cf7ef.m3u8"
+},
+{"episode":"5 бөлім",
+"is":"https://cache.libria.fun/videos/media/ts/4578/5/720/3f15d28b6f64bd0754c4cf8ca60e93bc.m3u8"
+},
+{"episode":"6 бөлім",
+"is":"https://cache.libria.fun/videos/media/ts/4578/6/720/9c1e9b04434170c6bbec896ed5d69c98.m3u8"
+},
+{"episode":"7 бөлім",
+"is":"https://cache.libria.fun/videos/media/ts/4578/7/720/a3531d2e4d7cba6c5a4a99d419381c0b.m3u8"
+},
+{"episode":"8 бөлім",
+"is":"https://cache.libria.fun/videos/media/ts/4578/8/720/f7444aea05286a7f0e44a1fb8f88c473.m3u8"
+},
+{"episode":"9 бөлім",
+"is":"https://cache.libria.fun/videos/media/ts/4578/9/720/66b943c7fb5525b6fc57de3e769a837b.m3u8"
+},
+{"episode":"10 бөлім",
+"is":"https://cache.libria.fun/videos/media/ts/4578/10/720/d19ede30e273ff9e768ef538f8f0b43f.m3u8"
+},
+{"episode":"11 бөлім",
+"is":"https://cache.libria.fun/videos/media/ts/4578/11/720/79b2c310260260944d6a32e51c551e64.m3u8"
+},
+{"episode":"12 бөлім",
+"is":"https://cache.libria.fun/videos/media/ts/4578/12/720/eca203988f29d35d8b04169b157274ec.m3u8"
+} ]
