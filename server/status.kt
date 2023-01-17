fd8ce55d767c7d59c8ee2039023572f98297f655
ウネル <117335242+unel-app@users.noreply.github.com>
2023-01-17 23:42:04 +0600

    Update status.kt

--- server/status.kt
+++ server/status.kt
@@ -1,5 +1,12 @@
 [ {
-"ava":"https://unel-app.github.io/api.unel.tv/server/profile/verified/developer_profile_avatarka.png",
+"ava"https://unel-app.github.io/api.unel.tv/server/profile/verified/developer_profile_avatarka.png",
+"user":"LisaDev",
+"title":"Test Post",
+"message":"Test Post:",
+"data":"17 қараша 2022ж - 23:41",
+"poster":"api.unel.tv/server/upload/img/https-unel-tv-openapi-server-upload-img-29379.png"
+},
+{"ava":"https://unel-app.github.io/api.unel.tv/server/profile/verified/developer_profile_avatarka.png",
 "user":"LisaDev",
 "title":"Это тестовый публикация!",
 "message":"Links: https://t.me/seele_off",
