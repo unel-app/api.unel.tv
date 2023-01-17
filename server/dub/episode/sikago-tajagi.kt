ea0828266f3aa145af81bf75f9bdd803d59d0d06
ウネル <117335242+unel-app@users.noreply.github.com>
2023-01-17 21:43:43 +0600

    Update sikago-tajagi.kt

--- server/dub/episode/sikago-tajagi.kt
+++ server/dub/episode/sikago-tajagi.kt
@@ -1,49 +1,49 @@
 [ {
 "episode":"1 бөлім",
-"is":"https://anifon-nurislam003.koyeb.app/AgADsS817"
+"is":"http://anifon.tk:9090/ea760f1015"
 },
 {"episode":"2 бөлім",
-"is":"https://anifon-nurislam003.koyeb.app/AgADry815"
+"is":"http://anifon.tk:9090/d668fe1014"
 },
 {"episode":"3 бөлім",
-"is":"https://anifon-nurislam003.koyeb.app/AgADzC821"
+"is":"http://anifon.tk:9090/fe14b21019"
 },
 {"episode":"4 бөлім",
-"is":"https://anifon-nurislam003.koyeb.app/AgAD1S825"
+"is":"http://anifon.tk:9090/cffa321023"
 },
 {"episode":"5 бөлім",
-"is":"https://anifon-nurislam003.koyeb.app/AgAD0S822"
+"is":"http://anifon.tk:9090/768a461022"
 },
 {"episode":"6 бөлім",
-"is":"https://anifon-nurislam003.koyeb.app/AgADMi882"
+"is":"http://anifon.tk:9090/7156521033"
 },
 {"episode":"7 бөлім",
-"is":"https://anifon-nurislam003.koyeb.app/AgADxy819"
+"is":"http://anifon.tk:9090/7957e31016"
 },
 {"episode":"8 бөлім",
-"is":"https://anifon-nurislam003.koyeb.app/AgAD9i841"
+"is":"http://anifon.tk:9090/7f6a8f1032"
 },
 {"episode":"9 бөлім",
-"is":"https://anifon-nurislam003.koyeb.app/AgAD6i829"
+"is":"http://anifon.tk:9090/696b5e1025"
 },
 {"episode":"10 бөлім",
-"is":"https://anifon-nurislam003.koyeb.app/AgAD9C839"
+"is":"http://anifon.tk:9090/37cf301030"
 },
 {"episode":"11 бөлім",
-"is":"https://anifon-nurislam003.koyeb.app/AgAD9y843"
+"is":"http://anifon.tk:9090/3d1fb21031"
 },
 {"episode":"12 бөлім",
-"is":"https://anifon-nurislam003.koyeb.app/AgAD7y837"
+"is":"http://anifon.tk:9090/d1912e1029"
 },
 {"episode":"13 бөлім",
-"is":"https://anifon-nurislam003.koyeb.app/AgAD7S835"
+"is":"http://anifon.tk:9090/86d1c61028"
 },
 {"episode":"14 бөлім",
-"is":"https://anifon-nurislam003.koyeb.app/AgAD7C833"
+"is":"http://anifon.tk:9090/598a8e1027"
 },
 {"episode":"15 бөлім",
-"is":"https://anifon-nurislam003.koyeb.app/AgAD6y831"
+"is":"http://anifon.tk:9090/b04a051026"
 },
 {"episode":"16 бөлім",
-"is":"https://anifon-nurislam003.koyeb.app/AgAD5y827"
+"is":"http://anifon.tk:9090/1ebedb1024"
 } ]
