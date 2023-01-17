7c280463f6a5641b4551b2c10547ace2b0085fd1
ウネル <117335242+unel-app@users.noreply.github.com>
2023-01-17 21:32:36 +0600

    Update status.kt

--- server/status.kt
+++ server/status.kt
@@ -2,7 +2,7 @@
 "ava":"https://unel-app.github.io/api.unel.tv/server/profile/verified/developer_profile_avatarka.png",
 "user":"LisaDev",
 "title":"Это тестовый публикация!",
-"message":"test, тест.",
+"message":"Links: https://t.me/seele_off",
 "data":"7 қараша 2022ж - 15:47",
 "poster":"null"
 } ]
