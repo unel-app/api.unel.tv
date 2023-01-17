1514f74da37221c85b5f34ae61dc52df1a995b25
ウネル <117335242+unel-app@users.noreply.github.com>
2023-01-17 21:17:37 +0600

    Update status.kt

--- server/status.kt
+++ server/status.kt
@@ -4,7 +4,6 @@
 "title":"Test",
 "message":"Test",
 "data":"17 қаңтар 2023ж - 20:55",
-"is_verified":"null",
 "poster":"https://cs9.pikabu.ru/post_img/big/2017/02/25/1/148797977117513249.jpg"
 },
 {"ava":"https://anixstatic.com/avatars/2c648016d7f40dd98f28cb3c4f333569e92bc2c1_EGZdOnoMGe.gif",
@@ -12,7 +11,6 @@
 "title":"New reputable",
 "message":"Working...",
 "data":"17 қаңтар 2023ж - 20:19",
-"is_verified":"null",
 "poster":"https://yt3.googleusercontent.com/y1uYu-62Uyw8GTYbxwPd5q8pVPGpc1usngaK4c0Q4cn0lsiVdIAmCmtHrsw9vYMoXmRpjI1bkw=s900-c-k-c0x00ffffff-no-rj"
 },
 {"ava":"https://unel-app.github.io/api.unel.tv/server/profile/verified/developer_profile_avatarka.png",
