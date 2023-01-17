62e70e12f65dead7e184503b266a7552cdcd5f59
ウネル <117335242+unel-app@users.noreply.github.com>
2023-01-17 20:25:51 +0600

    Update status.kt

--- server/status.kt
+++ server/status.kt
@@ -4,11 +4,13 @@
 "title":"New reputable",
 "message":"Working...",
 "data":"17 қаңтар 2023ж - 20:19",
-"is_verified":"false"
+"is_verified":"false",
+"poster":"https://yt3.googleusercontent.com/y1uYu-62Uyw8GTYbxwPd5q8pVPGpc1usngaK4c0Q4cn0lsiVdIAmCmtHrsw9vYMoXmRpjI1bkw=s900-c-k-c0x00ffffff-no-rj"
 },
 {"ava":"https://unel-app.github.io/api.unel.tv/server/profile/verified/developer_profile_avatarka.png",
 "user":"LisaDev",
 "title":"Это тестовый публикация!",
 "message":"test, тест.",
-"data":"7 қараша 2022ж - 15:47"
+"data":"7 қараша 2022ж - 15:47",
+"poster":"null"
 } ]
