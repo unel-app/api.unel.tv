8aa50c91ec6a15f68707b200d267ab257299f36c
ウネル <117335242+unel-app@users.noreply.github.com>
2023-01-17 22:01:32 +0600

    Create weightlifting-fairy-kim-bok-joo.kt

---
+++ server/dub/episode/weightlifting-fairy-kim-bok-joo.kt
@@ -0,0 +1,16 @@
+[ {
+"episode":"1 бөлім",
+"is":"http://anifon.tk:9090/7896271070"
+},
+{"episode":"2 бөлім",
+"is":"http://anifon.tk:9090/3bda3f1071"
+},
+{"episode":"3 бөлім",
+"is":"http://anifon.tk:9090/02361e1072"
+},
+{"episode":"4 бөлім",
+"is":"http://anifon.tk:9090/8256301073"
+},
+{"episode":"5 бөлім",
+"is":"http://anifon.tk:9090/efa7cd1075"
+} ]
