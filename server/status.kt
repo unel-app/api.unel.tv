7e9d365971fd45a45715ee850e616724822ff772
ウネル <117335242+unel-app@users.noreply.github.com>
2023-01-19 09:28:23 +0600

    Update status.kt

--- server/status.kt
+++ server/status.kt
@@ -4,7 +4,7 @@
 "title":"Test Post",
 "message":"Test Post:",
 "data":"17 қараша 2022ж - 23:41",
-"poster":"https://unel-app.github.io/api.unel.tv/server/upload/img/https-unel-tv-openapi-server-upload-img-29379.png"
+"poster":"https://unel-app.github.io/api.unel.tv/server/upload/img/https-setflix-tv-poster-289.png"
 },
 {"ava":"https://unel-app.github.io/api.unel.tv/server/profile/verified/developer_profile_avatarka.png",
 "user":"LisaDev",
